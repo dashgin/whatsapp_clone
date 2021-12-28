1078da90e7b7b1c7247ffb37fff12380f1424cf8
Dashgin <dsdsds2103@gmail.com>
2021-12-29 00:01:47 +0400

    changed asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -4,9 +4,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from . import routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django_asgi_app = get_asgi_application()
+
+from . import routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
